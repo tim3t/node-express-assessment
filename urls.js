801fe15cd78be2ed425576930cf40f79f8d47028
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-10-04 20:12:22 -0400

    fixed .txt naming

--- urls.js
+++ urls.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const axios = require('axios');
-const process = require('process');
+const path = require('path');
 
 function readUrlFile(path) {
 	fs.readFile(path, 'utf-8', async function callback(err, data) {
@@ -28,8 +28,11 @@ function readUrlFile(path) {
 }
 
 async function makeNewFile(res, err) {
-	// fs.writeFile(`${res.value.config.url}.txt`, res.value.data, (err) => {
-	fs.writeFile('File.txt', res.value.data, (err) => {
+	console.log(res.value.config.url);
+
+	const regex = /https:\/\/|http:\/\/?/gi;
+	let shortenedFile = res.value.config.url.replaceAll(regex, '').split('/')[0];
+	fs.writeFile(`${shortenedFile}.txt`, res.value.data, (err) => {
 		if (err) {
 			console.log(`Error writing file`, err);
 		}
